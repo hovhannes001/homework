4909ae4f0e31fc374fb3ff1b3e6d255a6b03f89c
hovhannes001 <hovhannesbaghdasaryan17@gmail.com>
2025-05-20 20:33:39 +0400

    some changes

--- classworks.js
+++ classworks.js
@@ -1,25 +1,32 @@
-class Jsonreader{
-    constructor(url){
-        this.url = url;
+function problem1(){
+    class Jsonreader{
+        constructor(url){
+            this.url = url;
+        }
+        async json() {
+            this.response = await fetch(this.url);
+            this.jsons = await this.response.json();
+        }
+        cmp(foo) {
+            return this.jsons.filter(foo);
+        }
     }
-    async json() {
-        this.response = await fetch(this.url);
-        this.jsons = await this.response.json();
+
+    let reader = new Jsonreader("https://jsonplaceholder.typicode.com/posts");
+    async function processJson() {
+        await reader.json();
+        let result = reader.cmp((elem) => {
+            return elem.userId > 5 && elem.userId < 7;
+        });
+        console.log(result);
     }
-    cmp(foo) {
-        return this.jsons.filter(foo);
-    }
+
+    processJson();
+}
+function problem2(){
+    
 }
 
-let reader = new Jsonreader("https://jsonplaceholder.typicode.com/posts");
-async function processJson() {
-    await reader.json();
-    let result = reader.cmp((elem) => {
-        return elem.userId > 5 && elem.userId < 7;
-    });
-    console.log(result);
-}
-
-processJson();
+problem1();
 
 
